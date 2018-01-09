c7f66fe5b1dadd00f6cd21805085de430a21c4eb
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-01-09 15:13:03 -0800

    add MySQL config and User model

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+using databasePractice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace databasePractice.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,19 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using databasePractice.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace databasePractice
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options => options
+                .UseMySql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
