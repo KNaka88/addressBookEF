dabfc2d9380f84ff2ec88b488da98f376d602905
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-01-10 10:44:25 -0800

    add UserForUpdateDto

---
+++ Dtos/UserForUpdateDto.cs
@@ -0,0 +1,9 @@
+namespace databasePractice.Dtos
+{
+    public class UserForUpdateDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }    
+    }
+}
