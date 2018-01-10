1d188b67fde200ed0651e989bb365731813461df
Koji Nakagawa <lightupthesky0627@gmail.com>
2018-01-10 10:58:15 -0800

    create AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace databasePractice.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class AuthController: Controller
+    {        
+    }
+}
